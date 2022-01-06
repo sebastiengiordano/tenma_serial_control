eb9ac66398bf63ec7e0184d5c149def9e88bd72f
Sébastien Giordano <sebastiengiordano@yahoo.fr>
2022-01-06 16:02:43 +0100

    BMS3Command: read debug tx added

--- bench/bms3_interface/bms3_command.py
+++ bench/bms3_interface/bms3_command.py
@@ -1,4 +1,4 @@
-from os.path import dirname, join as os_path_join, exists, isfile
+from os.path import dirname, join as os_path_join, isfile
 from os import listdir
 import serial
 import serial.tools.list_ports
@@ -15,10 +15,11 @@ class BMS3Command:
         self._command = CommandInterface()
         self._verbose = verbose
         self._set_firmware_folder()
+        self._port_list = self._get_port_list()
+        self._debug_tx_serial_port = None
 
     def connect_to_bms3(self):
-        port_list = self._get_port_list()
-        for port in port_list:
+        for port in self._port_list:
             try:
                 ser = self._connect_to_serial(
                     port=port,
@@ -26,15 +27,16 @@ class BMS3Command:
                     )
                 self._command.set_serial(ser)
                 self._command.cmdGetVersion()
-            except:
+                self._port_list.pop(port)
+            finally:
                 pass
         raise CmdException('Impossible de se connecter à la BMS3.')
 
-    def load_firmware(self, firmware_label):
+    def load_firmware(self, firmware_label: str) -> bool:
         # Check firmware avaibility
         firmware = firmware_label + '.bin'
         firmware_files_list = self._get_firmware_files_list
-        if not firmware in firmware_files_list:
+        if firmware not in firmware_files_list:
             raise CmdException(
                 f'This {firmware_label} firmware is not '
                 f'avaible in {self._firmware_folder_path}.')
@@ -52,17 +54,23 @@ class BMS3Command:
         else:
             return False
 
+    def read_debug_tx(self) -> str:
+        if self._debug_tx_serial is None:
+            self._debug_tx_serial = self._get_debug_tx_serial()
+        self._debug_tx_serial.readline()
+        return self._debug_tx_serial.readline().decode(encoding='utf-8')
+
     def _connect_to_serial(
             self,
-            port,
-            baudrate
-            )-> serial.Serial:
+            port: str,
+            baudrate: int
+            ) -> serial.Serial:
         timeout = 5
         bytesize = serial.EIGHTBITS
         stopbits = serial.STOPBITS_ONE
         parity = serial.PARITY_EVEN
-        xonxoff=0   # don't enable software flow control
-        rtscts=0    # don't enable RTS/CTS flow control
+        xonxoff = 0   # don't enable software flow control
+        rtscts = 0    # don't enable RTS/CTS flow control
 
         ser = serial.Serial(
             port=port,
@@ -92,7 +100,7 @@ class BMS3Command:
         sleep(0.5)
         return ser
 
-    def _get_port_list(self):
+    def _get_port_list(self) -> list(str):
         # Seek for all connected device
         available_serial_port = serial.tools.list_ports.comports()
         port_list = []
@@ -100,18 +108,34 @@ class BMS3Command:
             port_list.append(port)
         return port_list
 
+    def _get_debug_tx_serial(self):
+        for port in self._port_list:
+            try:
+                ser = self._connect_to_serial(
+                    port=port,
+                    baudrate=115200
+                    )
+                line = ser.readline()
+                line = line.decode(encoding='utf-8')
+                if int(line) >= 0:
+                    return ser
+            finally:
+                pass
+        raise CmdException(
+            'Impossible de lire sur les données du DEBUG_TX de la BMS3.')
+
     def _set_firmware_folder(self):
         module_dir = dirname(__file__)
         self._firmware_folder_path = os_path_join(
             module_dir,
             'bms3_firmwares')
 
-    def _get_firmware_files_list(self):
+    def _get_firmware_files_list(self) -> list(str):
         firmware_files_list = self._files_in_folder(
             self._firmware_folder_path)
         return firmware_files_list
 
-    def _files_in_folder(self, folder_path: str ) -> list:
+    def _files_in_folder(self, folder_path: str) -> list(str):
         '''Return the list of all files in folder_path and its subfolder'''
         files_in_folder_path = []
         # Loop on all files or directories inside folder_path
